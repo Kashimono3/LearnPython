d6a5263c47472ce33d065d03a47fbbedbff8bd33
Lê Viết Đăng Quang <112486652+Kashimono3@users.noreply.github.com>
2023-08-11 18:15:40 +0700

    lab2

---
+++ Lab2/Bai1.py
@@ -0,0 +1,53 @@
+class SinhVien:
+    truong = "Đại học Đà Lạt"
+    def _init_(self,maso: int, hoTen: str, ngaySinh: datetime) -> None:
+        self.__maSo = maSo
+        self.__hoTen = hoTen
+        self.__ngaySinh = ngaySinh
+        
+    @property
+    def maSo(self):
+        return self.__maSo
+
+    @maSo.setter
+    def maSo(self,maso):
+        if self.laMaSoHopLe(maso):
+            self.__maSo = maso
+    @staticmethod
+    def laMaSoHopLe(maso:int):
+        return len(str(maso)) == 7
+    @classmethod
+    def doiTenTruong(self,tenmoi):
+        self.truong = tenmoi
+    def __str__(self) -> str:
+        return f"{self.__maSo}\t{self.__hoTen}\t{self.__ngaySinh}"
+    def xuat(self)
+        print(f"{self.__maSo}\t{self.__hoTen}\t{self.__ngaySinh}")
+class DanhSachSv:
+    def __init__(self) -> None:
+        self.dssv = []
+    def themSinhVien(self,sv:SinhVien):
+        self.dssv.append(sv)
+    def xuat(self):
+        for sv in self.dssv:
+            print(sv)
+    def timSvTheoMssv(self,mssv:int):
+        return [sv for sv in self.dssv if sv.mssv == mssv]
+    def timVTSvTheoMssv(self,mssv:int):
+        for i in range(len(self.dssv)):
+            if self.dssv[i].mssv == mssv:
+                return i
+        return -1
+    def xoaSvTheoMssv(self,maSo: int) -> bool:
+        vt = self.timVTSvTheoMssv(maso)
+        if vt !=-1:
+            del self.dssv[vt]
+            return True
+        else:
+            return False
+    def timSvTheoTen(self,ten:str):
+        pass
+    def timSvSinhTruocNgay(self,ngay:datetime)
+        pass
+    def docDanhSach(text)
+        f = open(text,"r")
