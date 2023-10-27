82ad5a31979a13193d72e81cd892cf9c8fee8211
Lê Viết Đăng Quang <112486652+Kashimono3@users.noreply.github.com>
2023-10-27 23:23:09 +0700

    up

--- Lab2/Bài 1 +2/sinhvien.py
+++ Lab2/Bài 1 +2/sinhvien.py
@@ -23,37 +23,3 @@ class SinhVien:
         return f"{self.__maSo}\t{self.__hoTen}\t{self.__ngaySinh}"
     def xuat(self)
         print(f"{self.__maSo}\t{self.__hoTen}\t{self.__ngaySinh}")
-class DanhSachSv:
-    def __init__(self) -> None:
-        self.dssv = []
-    def themSinhVien(self,sv:SinhVien):
-        self.dssv.append(sv)
-    def xuat(self):
-        for sv in self.dssv:
-            print(sv)
-    def timSvTheoMssv(self,mssv:int):
-        return [sv for sv in self.dssv if sv.mssv == mssv]
-    def timVTSvTheoMssv(self,mssv:int):
-        for i in range(len(self.dssv)):
-            if self.dssv[i].mssv == mssv:
-                return i
-        return -1
-    def xoaSvTheoMssv(self,maSo: int) -> bool:
-        vt = self.timVTSvTheoMssv(maso)
-        if vt !=-1:
-            del self.dssv[vt]
-            return True
-        else:
-            return False
-    
-       pass
-       
-    def timSvSinhTruocNgay(self,ngay:datetime)
-        pass
-    def timSvTheoTen(self,ten:str):
-        listSV = []
-        if (self.soLuongSinhVien() > 0):
-         for sv in self.listSinhVien:
-            if (nam.upper() in sv.hoTen.upper()):
-                listSV.append(sv)
-        return listSV
